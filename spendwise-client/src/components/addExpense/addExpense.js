a564f8b59143382d263e65c56207653cb2932a45
Agaile Victor <avictor@silversky.com>
2020-06-05 13:02:22 +0800

    code cleanup

--- spendwise-client/src/components/addExpense/addExpense.js
+++ spendwise-client/src/components/addExpense/addExpense.js
@@ -4,7 +4,8 @@ import { Button, Modal, Form, Col } from 'react-bootstrap';
 import Switch from '@material-ui/core/Switch';
 import { FormGroup } from "@material-ui/core";
 import DateFnsUtils from '@date-io/date-fns';
-import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import { MuiPickersUtilsProvider, DatePicker } from '@material-ui/pickers';
+import * as moment from 'moment';
 
 export default class AddExpense extends Component {
 
@@ -17,53 +18,77 @@ export default class AddExpense extends Component {
         this.props.toggleReccuring();
     }
 
+    handleGetDateEntry = (e) => {
+        console.log(moment(e).format('L'));
+    }
+
+    submitForm = (e) => {
+        console.log('Form Data');
+        console.log(this.refs.amount.value);
+        console.log(this.refs.categoryId.value);
+        console.log(this.refs.expenseTypeId.value);
+        console.log(this.props.isRepeat);
+        console.log(this.refs.desc.value);
+    }
+
     render() {
         let datePicker, dateRangePicker;
         datePicker = (
-            <Form.Group controlId="ControlDatePicker1">
+            <Form.Group>
                 <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                    <KeyboardDatePicker
+                    <DatePicker
                         margin="normal"
                         id="date-picker-dialog"
                         label="Date of entry"
                         format="dd/MM/yyyy"
-                        value={new Date('2020-08-29')}
+                        value={moment().format('YYYY-MM-DD')}
+                        onChange={this.handleGetDateEntry}
                         KeyboardButtonProps={{
                             'aria-label': 'change date',
                         }}
+                        disablePast="true"
+                        maxDate={new Date('2020-08-29')}
+                        InputProps={{
+                            disableUnderline: true,
+                        }}
+                        invalidDateMessage="not valid"
                     />
                 </MuiPickersUtilsProvider>
             </Form.Group>
         );
 
         dateRangePicker = (
-            <FormGroup controlId="ControlDatePicker2">
+            <FormGroup>
                 <Form.Row>
                     <Col>
                         <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                            <KeyboardDatePicker
+                            <DatePicker
                                 margin="normal"
                                 id="date-picker-dialog"
                                 label="Start date"
                                 format="dd/MM/yyyy"
-                                value={new Date('2020-08-29')}
+                                value={moment().format('YYYY-MM-DD')}
+                                onChange={this.handleGetDateEntry}
                                 KeyboardButtonProps={{
                                     'aria-label': 'change date',
                                 }}
+                                disablePast="true"
                             />
                         </MuiPickersUtilsProvider>
                     </Col>
                     <Col>
                         <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                            <KeyboardDatePicker
+                            <DatePicker
                                 margin="normal"
                                 id="date-picker-dialog"
                                 label="End date"
                                 format="dd/MM/yyyy"
-                                value={new Date('2020-08-29')}
+                                value={moment().format('YYYY-MM-DD')}
+                                onChange={this.handleGetDateEntry}
                                 KeyboardButtonProps={{
                                     'aria-label': 'change date',
                                 }}
+                                disablePast="true"
                             />
                         </MuiPickersUtilsProvider>
                     </Col>
@@ -79,9 +104,9 @@ export default class AddExpense extends Component {
 
                 <Modal.Body>
                     <Form>
-                        <Form.Group controlId="ControlSelect1">
+                        <Form.Group>
                             <Form.Label>Category</Form.Label>
-                            <Form.Control as="select">
+                            <Form.Control as="select" ref="categoryId" className="my-1 mr-sm-2" custom>
                                 <option value="1">Utilities</option>
                                 <option value="2">Transportation</option>
                                 <option value="3">Internet</option>
@@ -91,18 +116,18 @@ export default class AddExpense extends Component {
                                 <option value="7">Others</option>
                             </Form.Control>
                         </Form.Group>
-                        <Form.Group controlId="ControlSelect2">
+                        <Form.Group>
                             <Form.Label>Expense Type</Form.Label>
-                            <Form.Control as="select">
+                            <Form.Control as="select" ref="expenseTypeId" className="my-1 mr-sm-2" custom>
                                 <option value="1">Earnings</option>
                                 <option value="2">Payment</option>
                             </Form.Control>
                         </Form.Group>
-                        <Form.Group controlId="ControlInput1">
+                        <Form.Group>
                             <Form.Label>Amount</Form.Label>
-                            <Form.Control type="text" placeholder="Enter amount" />
+                            <Form.Control required type="number" min='1' placeholder="Enter amount" ref="amount" />
                         </Form.Group>
-                        <FormGroup controlId="ControlSwitch1">
+                        <FormGroup>
                             <Form.Label>Reccuring Cost</Form.Label>
                             <Switch
                                 checked={this.props.isRepeat}
@@ -112,17 +137,17 @@ export default class AddExpense extends Component {
                                 inputProps={{ 'aria-label': 'primary checkbox' }}
                             />
                         </FormGroup>
-                        {this.props.isRepeat != true ? datePicker : dateRangePicker}
-                        <Form.Group controlId="ControlTextarea1">
+                        {this.props.isRepeat !== true ? datePicker : dateRangePicker}
+                        <Form.Group>
                             <Form.Label>Description</Form.Label>
-                            <Form.Control as="textarea" rows="3" />
+                            <Form.Control as="textarea" rows="3" ref="desc" />
                         </Form.Group>
                     </Form>
                 </Modal.Body>
 
                 <Modal.Footer>
                     <Button variant="secondary" onClick={this.closePopup}>Close</Button>
-                    <Button variant="primary">Save changes</Button>
+                    <Button variant="primary" onClick={this.submitForm}>Save changes</Button>
                 </Modal.Footer>
             </Modal>
         )
